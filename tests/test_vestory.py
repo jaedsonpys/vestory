132b3da8c3506d619bd20319592496ede7a59305
Jaedson Silva <contacanal798@gmail.com>
2022-02-23 00:09:07 -0300

    tests/test_vestory: adicionando teste "test_init_repo()"

--- tests/test_vestory.py
+++ tests/test_vestory.py
@@ -11,6 +11,14 @@ class TestVestory(SeqTest):
     def __init__(self):
         super().__init__()
 
+    def test_init_repo(self):
+        vestory.init_repo()
+
+        # verify
+        self.is_true(os.path.isdir('./.vestory'), 'Repo is not created')
+        self.is_true(os.path.isdir('./.vestory/changes'), 'Diretory "changes" not found')
+        self.is_true(os.path.isfile('./.vestory/vestory.config.json'), 'Config file not found')
+
 
 if __name__ == '__main__':
     TestVestory().run()
