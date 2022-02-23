a8b1d891b85734320efd89e9732833174c39169a
Jaedson Silva <contacanal798@gmail.com>
2022-02-23 00:02:04 -0300

    tests/test_vestory: inicializando classe de testes

---
+++ tests/test_vestory.py
@@ -0,0 +1,11 @@
+from pyseqtest import SeqTest
+import vestory
+
+
+class TestVestory(SeqTest):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == '__main__':
+    TestVestory().run()
