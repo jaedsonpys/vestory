54d82e14d15debf1d20b52f4df9ed54bfc6c4448
Jaedson Silva <contacanal798@gmail.com>
2022-02-23 19:13:37 -0300

    tests/test_vestory: adicionando teste "test_add_files"

--- tests/test_vestory.py
+++ tests/test_vestory.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 
@@ -21,7 +22,7 @@ class TestVestory(SeqTest):
         super().__init__()
 
     def setup(self):
-        self.files = os.listdir('test_files')
+        self.files = os.listdir('./tests/test_files')
 
     def test_init_repo(self):
         vestory.init_repo()
@@ -30,6 +31,16 @@ class TestVestory(SeqTest):
         self.is_true(os.path.isdir('./.vestory'), 'Repo is not created')
         self.is_true(os.path.isdir('./.vestory/changes'), 'Diretory "changes" not found')
         self.is_true(os.path.isfile('./.vestory/vestory.config.json'), 'Config file not found')
+
+    def test_add_files(self):
+        vestory.add_files(self.files)
+
+        # verify
+        with open('./.vestory/vestory.config.json', 'r') as config:
+            vestory_config: dict = json.load(config)
+
+        tracked_files = len(vestory_config['tracking_files'])
+        self.check_any_value(tracked_files, len(self.files), 'File not added')
 
 
 if __name__ == '__main__':
