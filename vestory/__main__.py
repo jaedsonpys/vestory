6422f6137dda8ae97858a94c754a6d61921e4887
Jaedson Silva <contacanal798@gmail.com>
2022-02-23 22:58:54 -0300

    vestory/__main__: adicionando flag para incialização de repositório

--- vestory/__main__.py
+++ vestory/__main__.py
@@ -1,17 +1,30 @@
+import os
 from argparse import ArgumentParser
-from .version_control import init_repo, add_files
+
+from version_control import add_files, init_repo
+
+EXEC_PATH = os.getcwd()
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('add', nargs='*', action='append')
-    parser.add_argument('init', action='store')
+    parser.add_argument('-i', action='store_true')  # -i is used to init repo
 
     args = parser.parse_args()
-    if args.init:
-        init_repo()
-    elif args.add:
-        add_files(args.add)
+    
+    if args.i:
+        # test informations
+        name = 'Elliot'
+        email = 'elliot@protonmail.com'
+
+        result = init_repo(name, email)
+
+        if not result:
+            print(f'\033[31mJá existe um repositório em "{EXEC_PATH}"\033[m')
+            return None
+        
+        print(f'\033[1;32mNovo repositório inicializado em "{EXEC_PATH}"!\033[m')
 
 
 main()
