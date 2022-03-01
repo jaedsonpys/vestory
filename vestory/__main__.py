347d10cee8e1654658cea889bbd8ee78f53f1b7e
Jaedson Silva <contacanal798@gmail.com>
2022-03-01 03:38:31 -0300

    vestory/__main__: verificando se o repositório existe

--- vestory/__main__.py
+++ vestory/__main__.py
@@ -8,7 +8,8 @@ from .version_control import (
     submit_change,
     get_changes,
     join_changes,
-    get_files_tracked
+    get_files_tracked,
+    _check_repo_exists
 )
 
 EXEC_PATH = os.getcwd()
@@ -33,95 +34,99 @@ def main():
     parser.add_flag('-c', 'Comment the change')
 
     args = parser.get_args()
+    repo_exists = _check_repo_exists()
 
     if args.init:
         # test informations
         name = 'Elliot'
         email = 'elliot@protonmail.com'
-
-        result = init_repo(name, email)
-
-        if not result:
+        
+        if repo_exists:
             print(f'\033[31mJá existe um repositório em "{EXEC_PATH}"\033[m')
         else:
+            init_repo(name, email)
             print(f'\033[1;32mNovo repositório inicializado em "{EXEC_PATH}"!\033[m')
-    elif args.add is not None:
-        if args.a:
-            files_to_add = []
-
-            for root, dir, files in os.walk('./'):
-                if not '.vestory' in root:
-                    for file in files:
-                        files_to_add.append(os.path.join(root, file))
-        else:
-            files_to_add = args.add
-
-        add_files(files_to_add)
-    elif args.submit is not None:
-        comment = args.c
-        if args.a:
-            files_to_submit = [file for file in get_files_tracked().keys()]
-        else:
-            files_to_submit = args.submit
-
-        if not comment:
-            print('error: a comment on the change is required. Use "-c."')
-            return None
-
-        submit_change(files_to_submit, comment)
-    elif args.log:
-        changes = get_changes()
-        changes_list = []
-
-        for change_id, change_info in changes.items():
-            changes_list.append((change_id, change_info))
-
-        # last changes first
-        changes_list.reverse()
-
-        for change_id, change_info in changes_list:
-            date = change_info.get('date')
-            comment = change_info.get('comment')
-            author = change_info.get('author')
-            author_email = change_info.get('author_email')
-
-            print(f'\033[33m{date} - {change_id}\033[m')
-            print(f'Author: {author} ({author_email})')
-            print(f'Comment: {comment}\n')
-    elif args.status:
-        changed_files = get_files_changed()
-
-        if not changed_files:
-            print('\033[32mno changes detected\033[m')
-        else:
-            print('files to submit:')
-            for file in changed_files:
-                print(f'\033[31m    changed: {file}\033[m')
-
-        print('\nuse "vestory submit -a" to submit changes.')
-        print('to add files, use "vestory add".')
-    elif args.join:
-        print('\033[33mwarning: the "join" command will'
-              'replace the current files.\033[m')
-
-        while True:
-            confirm = input('\033[1m> Do you wish to proceed? [y/n] ').strip().lower()
-            if confirm not in ('y', 'n'):
-                print('\033[31mSelect an option between "y" and "n"\033[m')
-                continue
-            else:
-                break
-
-        if confirm == 'n':
-            return None
-
-        joined_changes = join_changes()
-
-        for filepath, content in joined_changes.items():
-            file_lines = [line for line in content.values()]
-            write_file(filepath, ''.join(file_lines))
-            print(f'\033[32mfile "{filepath}" successfully completed\033[m')
     
-        print('\nDone.')
+    if repo_exists:
+        if args.add is not None:
+            if args.a:
+                files_to_add = []
+
+                for root, dir, files in os.walk('./'):
+                    if not '.vestory' in root:
+                        for file in files:
+                            files_to_add.append(os.path.join(root, file))
+            else:
+                files_to_add = args.add
+
+            add_files(files_to_add)
+        elif args.submit is not None:
+            comment = args.c
+            if args.a:
+                files_to_submit = [file for file in get_files_tracked().keys()]
+            else:
+                files_to_submit = args.submit
+
+            if not comment:
+                print('error: a comment on the change is required. Use "-c."')
+                return None
+
+            submit_change(files_to_submit, comment)
+        elif args.log:
+            changes = get_changes()
+            changes_list = []
+
+            for change_id, change_info in changes.items():
+                changes_list.append((change_id, change_info))
+
+            # last changes first
+            changes_list.reverse()
+
+            for change_id, change_info in changes_list:
+                date = change_info.get('date')
+                comment = change_info.get('comment')
+                author = change_info.get('author')
+                author_email = change_info.get('author_email')
+
+                print(f'\033[33m{date} - {change_id}\033[m')
+                print(f'Author: {author} ({author_email})')
+                print(f'Comment: {comment}\n')
+        elif args.status:
+            changed_files = get_files_changed()
+
+            if not changed_files:
+                print('\033[32mno changes detected\033[m')
+            else:
+                print('files to submit:')
+                for file in changed_files:
+                    print(f'\033[31m    changed: {file}\033[m')
+
+            print('\nuse "vestory submit -a" to submit changes.')
+            print('to add files, use "vestory add".')
+        elif args.join:
+            print('\033[33mwarning: the "join" command will'
+                'replace the current files.\033[m')
+
+            while True:
+                confirm = input('\033[1m> Do you wish to proceed? [y/n] ').strip().lower()
+                if confirm not in ('y', 'n'):
+                    print('\033[31mSelect an option between "y" and "n"\033[m')
+                    continue
+                else:
+                    break
+
+            if confirm == 'n':
+                return None
+
+            joined_changes = join_changes()
+
+            for filepath, content in joined_changes.items():
+                file_lines = [line for line in content.values()]
+                write_file(filepath, ''.join(file_lines))
+                print(f'\033[32mfile "{filepath}" successfully completed\033[m')
+        
+            print('\nDone.')
+    else:
+        print('\033[merror: .vestory repo not found\033[m')
 
     return None
