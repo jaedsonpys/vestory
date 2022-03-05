16605f1cba41a2d000b4cb56d02e412f0d3f2452
Jaedson Silva <contacanal798@gmail.com>
2022-03-04 21:34:34 -0300

    vestory/integrity: criando função para decodificar vários tokens

--- vestory/integrity.py
+++ vestory/integrity.py
@@ -20,3 +20,26 @@ def decode_token(token: str, key: str) -> Union[dict, bool]:
         return False
     else:
         return token_content
+
+
+def decode_tokens(tokens: list, key: str) -> list:
+    """Decode a list of tokens.
+
+    If any element of this list
+    is False, it means that the token is not
+    can be decoded.
+
+    :param tokens: List of tokens.
+    :type tokens: list
+    :param key: Key to decode
+    :type key: str
+    :return: Returns a list of decoded tokens.
+    :rtype: list
+    """
+
+    decoded_tokens = []
+    for t in tokens:
+        content = decode_token(t, key)
+        decode_token.append(content)
+    
+    return decoded_tokens
