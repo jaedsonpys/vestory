774a9b6474d9a42d7b1a8c9e8dd14aa80835ee36
Jaedson Silva <contacanal798@gmail.com>
2022-03-05 17:41:38 -0300

    vestory/exceptions: adicionando "InvalidChangeError"

--- vestory/exceptions.py
+++ vestory/exceptions.py
@@ -5,3 +5,8 @@ class Base(Exception):
 class RepoNotExistsError(Base):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
+
+
+class InvalidChangeError(Base):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
